eb4d1232977eb54fda29bd44fa61771c3915f2da
jkugalde <jcugaldecas@gmail.com>
2023-09-04 01:26:10 -0300

    intersection points between straight lines and box

--- src/G_scaffold_box_60_120.py
+++ src/G_scaffold_box_60_120.py
@@ -23,8 +23,6 @@ dn =  0.4 #diametro de boquilla en mm
 
 ln = round(A/(e+dn)) #lineas vacias por capa
 lf=ln+1 #lineas solidas por capa
-A = (e+dn)*ln+dn #longitud en x final
-dens = e*ln/A #densidad aproximada del modelo
 
 #impresion
 
@@ -46,12 +44,13 @@ zf=lh+ofz
 
 c=1 #sentido (-1 o 1)
 
-def makelayer(a,b,nl,theta): #con perimetro
+def makelayer(a,b,theta): #con perimetro
 
     #perimetro
 
     xf = X0-a/2
     yf = Y0+b/2
+    L=a
 
     gline(gcode,"G1 X"+str(xf)+" Y"+str(yf-b)+" E"+str(b*fp)+" F"+str(feedp))
     gline(gcode,"G1 X"+str(xf+a)+" Y"+str(yf)+" E"+str(a*fp)+" F"+str(feedp)) 
@@ -66,33 +65,18 @@ def makelayer(a,b,nl,theta): #con perimetro
     xf=X0-a/2
     yf=Y0+b/2
 
-    for i in range nl:
+    print(xf)
+    print(yf)
 
-    #ubicar cuadrante y definir direcciones 
+    gline(gcode,"G1 X"+str(xf)+" Y"+str(yf)+" F"+str(feedt)) #mover a punto inicial del patron  
 
-        dirx=0
-        diry=0
-
-        if(xf>X0) and (yf>Y0):
-            dirx=1
-            xf=xf-e
-        if(xf<X0) and (yf>Y0):
-            dirx=-1
-            xf=xf+e
-        if(xf<X0) and (yf<Y0):
-            diry=1
-            xf=xf+e
-        if(xf>X0) and (yf<Y0):
-            diry=-1
-            xf=xf-e
-
-        gline(gcode,"G1 X"+str(xf)+" Y"+str(yf)+" F"+str(feedt)) #mover a punto inicial del patron  
+    while(True):
 
         #interseccion de curvas
 
         #línea del patrón
 
-        m1=math.tan(theta)
+        m1=math.tan(theta+math.pi/2)
         a1=-m1
         b1=1
         c1=m1*xf-yf
@@ -121,7 +105,8 @@ def makelayer(a,b,nl,theta): #con perimetro
         c_r = [c1,c2,c3,c4,c5]
 
         inter = numpy.empty((2,4))
-        #par de líneas a intersectar, el criterio es minimizar la distancia entre puntos para encontrar el par
+
+        #par de líneas a intersectar, el criterio es encontrar las líneas que intersectan en el perímetro cuadrado
 
         for i in range(1,5):
 
@@ -129,41 +114,35 @@ def makelayer(a,b,nl,theta): #con perimetro
             num2=(a_r[i]*c_r[0]-a_r[0]*c_r[i])
             den=(a_r[0]*b_r[i]-a_r[i]*b_r[0])
             if den==0:
-                den=0.0001
+                den=-1
             inter[0,i-1]=num1/den
             inter[1,i-1]=num2/den
 
-        minval=1000000
-        min_index = [0,0]
+        point = numpy.empty((2,2))
 
-        dists=numpy.empty((4,4))
+        print(inter)
+        v=0
+
         for i in range(4):
-            for j in range(4):
-                dists[i,j]=math.sqrt(math.pow(inter[0][j]-inter[0][i],2)+math.pow(inter[1][j]-inter[1][i],2))
-                if dists[i,j]==0:
-                    dists[i,j]=1000000
-                if dists[i,j]<minval:
-                    minval=dists[i,j]
-                    min_index[0]=i
-                    min_index[1]=j
 
-        print(dists)
-        print(dists.min())
-        print(min_index[0],min_index[1])
+            if (inter[0,i]>=X0-a/2 and inter[0,i]<=X0+a/2) and (inter[1,i]<=Y0+b/2 and inter[1,i]>=Y0-b/2) and (inter[1,i]>0):
 
-        xi=inter[0][min_index[0]]
-        yi=inter[1][min_index[0]]
-        xf=inter[0][min_index[1]]
-        yf=inter[1][min_index[1]]
+                point[0,v] = inter[0,i]
+                point[1,v] = inter[1,i]
+                v=v+1      
+                if(v==2):
+                    break        
 
-        print(xi,yi)
-        print(xf,yf)
+        if(v==0):
+            break
 
-        L=0
-        gline(gcode,"G1 X"+str(xf)+" Y"+str(yf)+" E"+str(L*fp)+" F"+str(feedp))
+        L=math.sqrt(math.pow(point[0,0]-point[0,1],2)+math.pow(point[1,0]-point[1,1],2))
+        ##gline(gcode,"G1 X"+str(point[1,0])+" Y"+str(point[1,1])+" E"+str(L*fp)+" F"+str(feedp))
+        xf=xf+e
+        ##gline(gcode,"G1 X"+str(xf)+" Y"+str(yf)+" E"+str(L*fp)+" F"+str(feedt))
   
 
-with open('scaffold_box.gcode', 'w') as gcode:
+with open('scaffold_box.gcode','w') as gcode:
 
     gline(gcode,"G90")
     gline(gcode,"M83")
@@ -184,15 +163,14 @@ with open('scaffold_box.gcode', 'w') as gcode:
     gline(gcode,"G1 X"+str(xf)+" Y"+str(yf-B-skrt_dis)+" E"+str((A+skrt_dis)*fp)+" F"+str(feedp))
     gline(gcode,"G1 X"+str(xf+A+skrt_dis)+" Y"+str(yf)+" E"+str((A+skrt_dis)*fp)+" F"+str(feedp)) 
     gline(gcode,"G1 X"+str(xf)+" Y"+str(yf+B+skrt_dis)+" E"+str((A+skrt_dis)*fp)+" F"+str(feedp)) 
-    gline(gcode,"G1 X"+str(xf-A-skrt_dis)+" Y"+str(yf)+" E"+str((A+skrt_dis)*fp)+" F"+str(feedp)) 
-
+    gline(gcode,"G1 X"+str(xf-A-skrt_dis)+" Y"+str(yf)+" E"+str((A+skrt_dis)*fp)+" F"+str(feedp))
     gline(gcode,"G1 X"+str(xf+skrt_dis)+" Y"+str(yf-skrt_dis)+" F"+str(feedt))
         
 #estructura
 
-    for i in range(N):
+    for i in range(1):
 
-        makelayer(A,B,ln,math.pi*(i%3)*(1/3))
+        makelayer(A,B,math.pi*(i%3)*(1/3))
         zf=zf+lh
         gline(gcode,"G1 Z"+str(zf)) #siguiente capa    
     
